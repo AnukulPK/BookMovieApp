eabc79c205c04136d18896c632186e6adda7e639
AnukulPK <anukulpk2@gmail.com>
2021-06-21 14:50:14 +0530

    header integrated wip

--- src/screens/controller/Controller.js
+++ src/screens/controller/Controller.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../../common/header/Header";
 
 function Controller() {
-  return <div>Testing Git changes</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default Controller;
