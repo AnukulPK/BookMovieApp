9f18372aeaeb15aa3aed2cadd6e3953e0575e50c
AnukulPK <anukulpk2@gmail.com>
2021-06-22 12:01:59 +0530

    routing testing

--- src/Controller.js
+++ src/Controller.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Header from "./common/header/Header";
+import Home from "./screens/home/Home";
+import MovieDetails from "./screens/movieDetails/MovieDetails";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function Controller() {
   return (
-    <div>
-      <Header />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie-details" component={MovieDetails} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
