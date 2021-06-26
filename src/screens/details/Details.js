1e873b143aa08c6df6dc07da35fb4b4c48448aa8
AnukulPK <anukulpk2@gmail.com>
2021-06-26 20:53:09 +0530

    integrated react-youtube

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,19 +1,101 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import { Typography } from "@material-ui/core";
+import YouTube from "react-youtube";
+import "./Details.css";
 
 const Details = () => {
   let { id } = useParams();
   let [movieData, setMovieData] = useState("");
+  let [genres, setGenres] = useState([]);
+  let [youtubeUrl, setYouttubeUrl] = useState("");
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:8085/api/v1/movies/${id}`)
-      .then((response) => setMovieData(response.data));
+    axios.get(`http://localhost:8085/api/v1/movies/${id}`).then((response) => {
+      setMovieData(response.data);
+      setGenres(response.data.genres);
+      setYouttubeUrl(response.data.trailer_url);
+    });
   }, []);
 
-  console.log(movieData);
-  return <div>Movie Details Page</div>;
+  var releaseDate = new Date(movieData.release_date).toDateString();
+
+  let youtubeId = youtubeUrl.split("=")[1];
+
+  let opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+      origin: "http://localhost:3000",
+    },
+  };
+
+  return (
+    <div className="details-content">
+      <Typography style={{ margin: "10px" }}>
+        <Link to="/" className="back-link">
+          <span className="back-to-home">&#60; Back to Home</span>
+        </Link>
+      </Typography>
+      <div className="main-content">
+        <div>
+          <img src={movieData.poster_url} alt={movieData.title} />
+        </div>
+        <div className="mid-container">
+          <Typography variant="h2" component="h2">
+            {movieData.title}
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            <b>Genre: </b>
+            {genres.map((genre) => `${genre}, `)}
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            <b>Duration: </b>
+            {movieData.duration}
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            <b>Release Date: </b>
+            {releaseDate}
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            <b>Rating: </b>
+            {movieData.rating}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            gutterBottom
+            style={{ marginTop: "16px" }}
+          >
+            <b>
+              Plot:{" "}
+              <a href={movieData.wiki_url} target="_blank">
+                (Wiki Link)
+              </a>
+            </b>
+            {" " + movieData.storyline}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            gutterBottom
+            style={{ marginTop: "16px" }}
+          >
+            <b>Trailer:</b>
+            <YouTube
+              videoId={youtubeId}
+              opts={opts}
+              onReady={(event) => {
+                event.target.pauseVideo();
+              }}
+            />
+            ;
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
